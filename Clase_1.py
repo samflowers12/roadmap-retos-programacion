1ec398c6c15b9c326ab0cfe14c0258883c6a9316
samflowers12 <samuelalejandroflores12@gmail.com>
2024-09-27 12:21:42 -0400

    Create Clase_1.py

---
+++ Clase_1.py
@@ -0,0 +1,26 @@
+#https://python.org
+
+'''
+esto es
+un comentario 
+multlinea
+'''
+
+my_variable='Esta es mi variable'
+my_others_variable='Esta es mi otra variable
+
+my_int=4
+my_float=1.5
+my_str='Hola mundo'
+my_other_str='Mi otro hola Mundo' 
+my_bool=true
+my_bool=false
+my_compless=7
+print('Hola Mundo')
+
+prin(type(my_int))
+print(type(my_bool))
+print(type(my_str))
+print(type(my_float))
+
+
